4df11b801aa670722cbf1be5501d418c4a6fb31c
harukanoo <harukanox2@gmail.com>
2018-12-07 13:48:59 +0700

    create a redirect page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
-import {  Route } from 'react-router-dom';
+import {  Route,Redirect } from 'react-router-dom';
 import './App.css';
 import routes from './routes'
 import Header from './Components/Shared/Header';
@@ -10,7 +10,9 @@ class App extends Component {
   render() {
     return (
       <Router>
+        
         <div>
+        <Redirect from="/" to="/ghibli" />
           <Header />
           {routes.map(route => (
             <Route exact path={route.path} component={route.component} />
